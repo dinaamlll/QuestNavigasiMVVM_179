673e4ffc9945bfcae6bc4fb39bc0f0a8eaa44984
dinaamlll <dinamaliaa17@gmail.com>
2024-11-16 22:12:56 +0700

    Menambahkan val NIM, no hp, email ke dalam data class Mahasiswa yang ada pada Model

--- app/src/main/java/com/example/arsitekturmvvm/model/Mahasiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/Mahasiswa.kt
@@ -3,6 +3,9 @@ package com.example.arsitekturmvvm.model
 data class Mahasiswa(
     val nama : String ="",
     val gender : String ="",
+    val NIM : String = "",
     val alamat : String ="",
+    val email : String ="",
+    val noHp : String = ""
 
 ) //val karena datanya hanya 1 (static)
